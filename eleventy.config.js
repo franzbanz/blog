2cf0bfb9306543c46f3999beaa53867eac84f77a
franzbanz <franz@koehler-kn.de>
2025-03-03 21:18:00 +0100

    added .gitignore

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,7 @@
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("pdfs")
+    eleventyConfig.addCollection("pdfs", function(collectionApi) {
+      return collectionApi.getAll().filter(item => item.inputPath.endsWith(".pdf"));
+    });
   };
