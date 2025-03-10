c73767aeddc7b02028a4fcf03f7daff3ee2e8741
franzbanz <franz@koehler-kn.de>
2025-03-10 23:43:50 +0100

    besseres layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,4 @@
 export default function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("style.css");
-    eleventyConfig.addPassthroughCopy("pdfs")
+    eleventyConfig.addPassthroughCopy("src/style.css");
+    eleventyConfig.addPassthroughCopy("src/pdfs")
   };
